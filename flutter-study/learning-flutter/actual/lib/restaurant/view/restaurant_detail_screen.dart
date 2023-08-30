b70b0e450926c548d089fef3399ae3d0ed1d6b52
idbdyoung <idbd.young@gmail.com>
2023-08-31 08:47:22 +0900

    sliver list 연습

---
+++ flutter-study/learning-flutter/actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -0,0 +1,77 @@
+import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/product/component/product_card.dart';
+import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantDetailScreen extends StatelessWidget {
+  const RestaurantDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: '불타는 떡볶이',
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
+        ],
+      ),
+    );
+  }
+
+  renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이', '맛있음', '치즈'],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 3000,
+        ratings: 4.76,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(fontSize: 1),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+}
+
+// Column(
+// children: [
+
+// Padding(
+// padding: const EdgeInsets.symmetric(horizontal: 16.0),
+// child: ProductCard(),
+// ),
+// ],
+// ),
